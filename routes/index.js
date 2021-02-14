de6e5ad201cf5fe490f6ca14e426d4c27f6cfeb4
Arpit <arpit@Arpits-MacBook-Pro.local>
2021-02-13 23:02:42 -0500

    made some minor changes

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
 });
 
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
 router.get('/about', function(req, res, next) {
   res.render('index', { title: 'About' });
 });
