27148338dd2384e312656c9f9a0c3c1fc987c55f
Arpit <arpit@Arpits-MacBook-Pro.local>
2021-02-13 23:44:43 -0500

    made some minor changes

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,27 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: "Home" });
 });
 
 router.get('/home', function(req, res, next) {
-  res.render('index', { title: 'Home' });
+  res.render('index', { title: "Home" });
 });
 
 router.get('/about', function(req, res, next) {
-  res.render('index', { title: 'About' });
+  res.render('index', { title: "About" });
 });
 
 router.get('/services', function(req, res, next) {
-  res.render('index', { title: 'Services' });
+  res.render('index', { title: "Services" });
 });
 
 router.get('/contact', function(req, res, next) {
-  res.render('index', { title: 'Contact' });
+  res.render('index', { title: "Contact" });
 });
 
 router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products' });
+  res.render('index', { title: "Products" });
 });
 
 module.exports = router;
